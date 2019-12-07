e4bf19222258eb2d3cdd21d837b0f58d53efd454
Ilya Dr <gidegode1940@gmail.com>
2019-12-07 15:53:40 +0300

    init project with core dependencies

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default () => (
+  <View>
+    <Text>123</Text>
+    <Icon name="rocket" size={30} color="#900" />
+  </View>
+);
